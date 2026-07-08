ee796cfdcf79eb2204f328b242e851c655f3ecda
Mattias Söderberg <mattes.email@gmail.com>
2026-07-08 11:21:42 +0200

    Add Terraform configuration for GCS backend and application module

---
+++ prod/main.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "gcs" {
+    bucket = var.terraform_state_bucket
+    prefix = "${var.terraform_state_prefix}/prod"
+  }
+}
+
+module "app" {
+  source      = "../modules/app"
+  environment = "prod"
+}
