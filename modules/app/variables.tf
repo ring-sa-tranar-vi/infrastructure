3ce155d014c0d38e161832d5a3296be1e1a23f0e
Mattias Söderberg <mattes.email@gmail.com>
2026-07-13 14:15:31 +0200

    Remove Grafana OTLP variables from environment configurations

--- modules/app/variables.tf
+++ modules/app/variables.tf
@@ -45,18 +45,6 @@ variable "db_url" {
   type        = string
 }
 
-variable "grafana_otlp_url" {
-  description = "URL for Grafana OTLP"
-  type        = string
-  sensitive   = true
-}
-
-variable "grafana_otlp_auth" {
-  description = "Basic Auth for Grafana OTLP"
-  type        = string
-  sensitive   = true
-}
-
 variable "clerk_jwt_issuer_uri" {
   description = "Clerk JWT Issuer URI"
   type        = string
--- staging/variables.tf
+++ staging/variables.tf
@@ -1,15 +1,3 @@
-variable "grafana_otlp_url" {
-  description = "URL for Grafana OTLP"
-  type        = string
-  sensitive   = true
-}
-
-variable "grafana_otlp_auth" {
-  description = "Basic Auth for Grafana OTLP"
-  type        = string
-  sensitive   = true
-}
-
 variable "clerk_jwt_issuer_uri" {
   description = "Clerk JWT Issuer URI"
   type        = string
