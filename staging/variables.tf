7287630e9537e25e296585a365d0e178b0b9c4e4
Mattias Söderberg <mattes.email@gmail.com>
2026-07-13 13:11:48 +0200

    Fix description language for Grafana OTLP variables in prod and staging environments

--- staging/variables.tf
+++ staging/variables.tf
@@ -1,11 +1,11 @@
 variable "grafana_otlp_url" {
-  description = "URL för Grafana OTLP"
+  description = "URL for Grafana OTLP"
   type        = string
   sensitive   = true
 }
 
 variable "grafana_otlp_auth" {
-  description = "Basic Auth för Grafana OTLP"
+  description = "Basic Auth for Grafana OTLP"
   type        = string
   sensitive   = true
 }
