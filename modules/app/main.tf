b08715c074b7409f314eed279df2066957785ef1
Mattias Söderberg <mattes.email@gmail.com>
2026-07-22 18:13:07 +0200

    Add lifecycle block to ignore changes in Cloud Run service image

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -184,6 +184,12 @@ resource "google_cloud_run_v2_service" "backend" {
       }
     }
   }
+
+  lifecycle {
+    ignore_changes = [
+      template[0].containers[0].image,
+    ]
+  }
 }
 
 resource "google_cloud_run_v2_service_iam_binding" "public_acess" {
