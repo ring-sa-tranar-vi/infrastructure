59f8d027b988453affb73e92ff903378ae0b7558
Mattias Söderberg <mattes.email@gmail.com>
2026-07-23 14:28:49 +0200

    Add cors_allowed_origins variable for staging and production environments

--- prod/main.tf
+++ prod/main.tf
@@ -20,4 +20,5 @@ module "app" {
   environment = "prod"
   db_url      = "jdbc:postgresql://ep-sweet-unit-as7v1s28-pooler.c-4.eu-central-1.aws.neon.tech/production?sslmode=require&channelBinding=require"
   clerk_jwt_issuer_uri = var.clerk_jwt_issuer_uri
+  cors_allowed_origins = ["https://prod-ringsatranarvi-app.web.app"]
 }
--- staging/main.tf
+++ staging/main.tf
@@ -20,4 +20,5 @@ module "app" {
   environment = "staging"
   db_url      = "jdbc:postgresql://ep-sweet-unit-as7v1s28-pooler.c-4.eu-central-1.aws.neon.tech/staging?sslmode=require&channelBinding=require"
   clerk_jwt_issuer_uri = var.clerk_jwt_issuer_uri
+  cors_allowed_origins = ["https://staging-ringsatranarvi-app.web.app"]
 }
