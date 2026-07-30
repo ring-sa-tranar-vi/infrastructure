c76e3e4f376971d16c634aaca86523da8b128a16
Jing <70271139+W3ndig0u0@users.noreply.github.com>
2026-07-30 15:00:44 +0200

    Update main.tf

--- staging/main.tf
+++ staging/main.tf
@@ -16,9 +16,18 @@ provider "google-beta" {
 }
 
 module "app" {
-  source      = "../modules/app"
-  environment = "staging"
-  db_url      = "jdbc:postgresql://ep-sweet-unit-as7v1s28-pooler.c-4.eu-central-1.aws.neon.tech/staging?sslmode=require&channelBinding=require"
-  clerk_jwt_issuer_uri = var.clerk_jwt_issuer_uri
-  cors_allowed_origins = ["https://staging-ringsatranarvi-app.web.app"]
+  source = "../modules/app"
+
+  environment                   = var.environment
+  project_id                    = var.project_id
+  default_region                = var.default_region
+  backend_location              = var.backend_location
+  cloud_run_cpu                 = var.cloud_run_cpu
+  cloud_run_memory              = var.cloud_run_memory
+  service_account_id            = var.service_account_id
+  db_url                        = var.db_url
+  clerk_jwt_issuer_uri          = var.clerk_jwt_issuer_uri
+  gcp_storage_bucket_name       = var.gcp_storage_bucket_name
+  cors_allowed_origins          = var.cors_allowed_origins
+  firebase_service_account_json = var.firebase_service_account_json
 }
