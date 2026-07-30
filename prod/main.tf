2a1d0374e084718b97c045df2014bf4a5cc577d0
Jing <70271139+W3ndig0u0@users.noreply.github.com>
2026-07-30 15:28:05 +0200

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -15,25 +15,11 @@ provider "google-beta" {
   region  = "europe-north2"
 }
 
-import {
-  to = module.app.google_secret_manager_secret.db_password
-  id = "projects/49973934534/secrets/prod-neon-db-password"
-}
-
-import {
-  to = module.app.google_secret_manager_secret.gemini_api_key
-  id = "projects/49973934534/secrets/prod-gemini-api-key"
-}
-
-import {
-  to = module.app.google_secret_manager_secret.openai_api_key
-  id = "projects/49973934534/secrets/prod-openai-api-key"
-}
-
 module "app" {
   source      = "../modules/app"
   environment = "prod"
   db_url      = "jdbc:postgresql://ep-sweet-unit-as7v1s28-pooler.c-4.eu-central-1.aws.neon.tech/production?sslmode=require&channelBinding=require"
   clerk_jwt_issuer_uri = var.clerk_jwt_issuer_uri
   cors_allowed_origins = ["https://prod-ringsatranarvi-app.web.app"]
+  firebase_service_account_json = var.firebase_service_account_json
 }
