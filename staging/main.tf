bf151ce74d27fa299e82fe459e611f562170c06a
Jing <70271139+W3ndig0u0@users.noreply.github.com>
2026-07-30 16:31:58 +0200

    Update main.tf

--- staging/main.tf
+++ staging/main.tf
@@ -20,6 +20,6 @@ module "app" {
   environment = "staging"
   db_url      = "jdbc:postgresql://ep-sweet-unit-as7v1s28-pooler.c-4.eu-central-1.aws.neon.tech/staging?sslmode=require&channelBinding=require"
   clerk_jwt_issuer_uri = var.clerk_jwt_issuer_uri
-  cors_allowed_origins = ["https://prod-ringsatranarvi-app.web.app"]
+  cors_allowed_origins = ["https://staging-ringsatranarvi-app.web.app"]
   firebase_service_account_json = var.firebase_service_account_json
 }
