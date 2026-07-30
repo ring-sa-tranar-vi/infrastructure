21adcd0c135c25621f3f546725d947aa58a1f23c
Jing <70271139+W3ndig0u0@users.noreply.github.com>
2026-07-30 16:31:35 +0200

    Update main.tf

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -117,7 +117,7 @@ data "google_secret_manager_secret" "grafana_otlp_auth" {
 resource "google_cloud_run_v2_service" "backend" {
   name     = "${var.environment}-backend-service"
   location = var.backend_location
-
+  deletion_protection = false
   template {
     service_account = data.google_service_account.sa_account.email
 
