53c99e1a3f0a1f7825fbbe533831a8fde39b1808
Jing <70271139+W3ndig0u0@users.noreply.github.com>
2026-07-30 15:02:21 +0200

    Update main.tf

--- staging/main.tf
+++ staging/main.tf
@@ -18,16 +18,9 @@ provider "google-beta" {
 module "app" {
   source = "../modules/app"
 
-  environment                   = var.environment
-  project_id                    = var.project_id
-  default_region                = var.default_region
-  backend_location              = var.backend_location
-  cloud_run_cpu                 = var.cloud_run_cpu
-  cloud_run_memory              = var.cloud_run_memory
-  service_account_id            = var.service_account_id
+  environment                   = "staging"
   db_url                        = var.db_url
   clerk_jwt_issuer_uri          = var.clerk_jwt_issuer_uri
-  gcp_storage_bucket_name       = var.gcp_storage_bucket_name
-  cors_allowed_origins          = var.cors_allowed_origins
   firebase_service_account_json = var.firebase_service_account_json
+  cors_allowed_origins          = ["https://staging-ringsatranarvi-app.web.app", "http://localhost:5173"]
 }
