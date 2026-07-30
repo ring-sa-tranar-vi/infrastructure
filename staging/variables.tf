3124267e1ac4d883e0fd5b7c09cf58931be22975
Jing <70271139+W3ndig0u0@users.noreply.github.com>
2026-07-30 15:02:35 +0200

    Update variables.tf

--- staging/variables.tf
+++ staging/variables.tf
@@ -1,5 +1,17 @@
+variable "db_url" {
+  description = "Database URL"
+  type        = string
+  sensitive   = true
+}
+
 variable "clerk_jwt_issuer_uri" {
   description = "Clerk JWT Issuer URI"
+  type        = string
+  sensitive   = true
+}
+
+variable "firebase_service_account_json" {
+  description = "Firebase Service Account JSON"
   type        = string
   sensitive   = true
 }
