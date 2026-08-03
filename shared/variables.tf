10b923765c75800abb9d4a71ee3fc6d4af247178
Mattias Söderberg <mattes.email@gmail.com>
2026-08-03 10:05:04 +0200

    Remove Firebase Service Account JSON variable from variables.tf

--- shared/variables.tf
+++ shared/variables.tf
@@ -37,9 +37,3 @@ variable "cors_allowed_origins" {
     "http://localhost:5173"
   ]
 }
-
-variable "firebase_service_account_json" {
-  description = "Raw JSON contents of the Firebase Service Account key"
-  type        = string
-  sensitive   = true
-}
