22535079c77097113a3f49be12d49bfb35b12aed
Mattias Söderberg <mattes.email@gmail.com>
2026-08-03 10:27:59 +0200

    Remove unused db_url variable from variables.tf in prod and staging

--- prod/variables.tf
+++ prod/variables.tf
@@ -1,9 +1,3 @@
-variable "db_url" {
-  description = "Database URL"
-  type        = string
-  sensitive   = true
-}
-
 variable "clerk_jwt_issuer_uri" {
   description = "Clerk JWT Issuer URI"
   type        = string
